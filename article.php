cbd72f1ecd55625cf0e7561d2420598a59dc6f50
iMAC <iMAC@imac-de-carles.home>
2018-09-12 01:28:10 +0200

    article new design mobile working on it

--- article.php
+++ article.php
@@ -28,6 +28,9 @@
         <section id="subheader">
             <span>Vestibulum ante ipsum primis in faucibus orci luctus et ultries pueser cubila curae.</span>
         </section>
+        <section id="header-img">
+            <img src="assets/img/articles/usabilidad.jpg" alt="Usabiliad / UX" />
+        </section>
         <section id="content">
             <p>
                 Al hablar de <strong>usabilidad / UX</strong> se suelen escuchar ciertas pautas que nos ayudan a saber
@@ -111,15 +114,19 @@
                 podemos conseguir justo lo contrario.
             </p>
 
+            <div class="fotoby">
+                <span>Fotografía de portada de:</span>
+                <a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@medbadrc?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Med Badr  Chemmaoui"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-1px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M20.8 18.1c0 2.7-2.2 4.8-4.8 4.8s-4.8-2.1-4.8-4.8c0-2.7 2.2-4.8 4.8-4.8 2.7.1 4.8 2.2 4.8 4.8zm11.2-7.4v14.9c0 2.3-1.9 4.3-4.3 4.3h-23.4c-2.4 0-4.3-1.9-4.3-4.3v-15c0-2.3 1.9-4.3 4.3-4.3h3.7l.8-2.3c.4-1.1 1.7-2 2.9-2h8.6c1.2 0 2.5.9 2.9 2l.8 2.4h3.7c2.4 0 4.3 1.9 4.3 4.3zm-8.6 7.5c0-4.1-3.3-7.5-7.5-7.5-4.1 0-7.5 3.4-7.5 7.5s3.3 7.5 7.5 7.5c4.2-.1 7.5-3.4 7.5-7.5z"></path></svg></span><span style="display:inline-block;padding:2px 3px">Med Badr  Chemmaoui</span></a>
+            </div>
+
         </section>
         <section id="footer" class="dark">
-            <div class="category featured">
-                <span>Categoría:</span> <a href="#">Usabilidad / UX</a>
-            </div>
             <div class="tags-list featured type2">
                 <span>Etiquetas:</span><br/>
                 <a href="#">Usabilidad</a>
             </div>
+
+            
         </section>
 
         <section id="comments">
